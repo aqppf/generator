9b102021a67d6f820a6e07320beee6a14d3f2af8
aqppf <aqppf@163.com>
2020-09-27 17:21:09 +0800

    dfdf

--- src/main/java/com/item/gen/plugins/CustomJavaMapperMethodGenerator.java
+++ src/main/java/com/item/gen/plugins/CustomJavaMapperMethodGenerator.java
@@ -1,8 +1,10 @@
 package com.item.gen.plugins;
 
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.api.dom.java.Interface;
 import org.mybatis.generator.api.dom.java.JavaVisibility;
@@ -16,6 +18,7 @@ public class CustomJavaMapperMethodGenerator extends AbstractJavaMapperMethodGen
 	public void addInterfaceElements(Interface interfaze) {
 		
 		addInterfaceSelectByCondition(interfaze);
+		addInterfaceSelectByPrimaryIds(interfaze);
 	}
 	
 	private void addInterfaceSelectByCondition(Interface interfaze) {
@@ -51,116 +54,50 @@ public class CustomJavaMapperMethodGenerator extends AbstractJavaMapperMethodGen
 		}
 	}
 	
-	@SuppressWarnings("unused")
-	private void addInterfaceFind(Interface interfaze) {
+	
+	private void addInterfaceSelectByPrimaryIds(Interface interfaze) {
+		
+		List<IntrospectedColumn> id = introspectedTable.getPrimaryKeyColumns();
+		
+		if (id.size() != 1) {
+			return;
+		}
+		
 		// 先创建import对象
 		Set<FullyQualifiedJavaType> importedTypes = new TreeSet<FullyQualifiedJavaType>();
 		// 添加Lsit的包
 		importedTypes.add(FullyQualifiedJavaType.getNewListInstance());
+		
 		// 创建方法对象
 		Method method = new Method();
-		// 设置该方法为public
+		
 		method.setVisibility(JavaVisibility.PUBLIC);
-		// 设置返回类型是List
-		FullyQualifiedJavaType returnType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		// 方法对象设置返回类型对象
+		
+		FullyQualifiedJavaType returnType = FullyQualifiedJavaType.getNewListInstance();
+		returnType.addTypeArgument(new FullyQualifiedJavaType(introspectedTable.getBaseRecordType()));
 		method.setReturnType(returnType);
-		// 设置方法名称为我们在IntrospectedTable类中初始化的 “selectByObject”
-		method.setName("find");
-
+		
+		String idName = id.get(0).getJavaProperty();
+		idName = idName.substring(0, 1).toUpperCase() + idName.substring(1);
+		method.setName("selectBy"+idName+"List");
+		
 		// 设置参数类型是对象
-		FullyQualifiedJavaType parameterType;
-		parameterType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		// import参数类型对象
-		importedTypes.add(parameterType);
+		FullyQualifiedJavaType parameterType=FullyQualifiedJavaType.getNewListInstance();
+		parameterType.addTypeArgument(id.get(0).getFullyQualifiedJavaType());
+		
 		// 为方法添加参数，变量名称record
-		method.addParameter(new Parameter(parameterType, "record")); //$NON-NLS-1$
-		//
+		method.addParameter(new Parameter(parameterType, "list"));
+		
 		addMapperAnnotations(interfaze, method);
 		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
 		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable)) {
 			interfaze.addImportedTypes(importedTypes);
 			interfaze.addMethod(method);
 		}
+		
 	}
 	
-	@SuppressWarnings("unused")
-	private void addInterfaceList(Interface interfaze) {
-		// 先创建import对象
-		Set<FullyQualifiedJavaType> importedTypes = new TreeSet<FullyQualifiedJavaType>();
-		// 添加Lsit的包
-		importedTypes.add(FullyQualifiedJavaType.getNewListInstance());
-		// 创建方法对象
-		Method method = new Method();
-		// 设置该方法为public
-		method.setVisibility(JavaVisibility.PUBLIC);
-		// 设置返回类型是List
-		FullyQualifiedJavaType returnType = FullyQualifiedJavaType.getNewListInstance();
-		FullyQualifiedJavaType listType;
-		// 设置List的类型是实体类的对象
-		listType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		importedTypes.add(listType);
-		// 返回类型对象设置为List
-		returnType.addTypeArgument(listType);
-		// 方法对象设置返回类型对象
-		method.setReturnType(returnType);
-		// 设置方法名称为我们在IntrospectedTable类中初始化的 “selectByObject”
-		method.setName("list");
-
-		// 设置参数类型是对象
-		FullyQualifiedJavaType parameterType;
-		parameterType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		// import参数类型对象
-		importedTypes.add(parameterType);
-		// 为方法添加参数，变量名称record
-		method.addParameter(new Parameter(parameterType, "record")); //$NON-NLS-1$
-		//
-		addMapperAnnotations(interfaze, method);
-		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
-		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable)) {
-			interfaze.addImportedTypes(importedTypes);
-			interfaze.addMethod(method);
-		}
-	}
 	
-	@SuppressWarnings("unused")
-	private void addInterfacePageList(Interface interfaze) {
-		// 先创建import对象
-		Set<FullyQualifiedJavaType> importedTypes = new TreeSet<FullyQualifiedJavaType>();
-		// 添加Page的包
-		importedTypes.add(FullyQualifiedJavaType.getBooleanPrimitiveInstance());
-		// 创建方法对象
-		Method method = new Method();
-		// 设置该方法为public
-		method.setVisibility(JavaVisibility.PUBLIC);
-		// 设置返回类型是List
-		FullyQualifiedJavaType returnType = FullyQualifiedJavaType.getNewListInstance();
-		FullyQualifiedJavaType listType;
-		// 设置List的类型是实体类的对象
-		listType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		importedTypes.add(listType);
-		// 返回类型对象设置为List
-		returnType.addTypeArgument(listType);
-		// 方法对象设置返回类型对象
-		method.setReturnType(returnType);
-		// 设置方法名称为我们在IntrospectedTable类中初始化的 “selectByObject”
-		method.setName("pageList");
-
-		// 设置参数类型是对象
-		FullyQualifiedJavaType parameterType;
-		parameterType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
-		// import参数类型对象
-		importedTypes.add(parameterType);
-		// 为方法添加参数，变量名称record
-		method.addParameter(new Parameter(parameterType, "record")); //$NON-NLS-1$
-		//
-		addMapperAnnotations(interfaze, method);
-		context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
-		if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, interfaze, introspectedTable)) {
-			interfaze.addImportedTypes(importedTypes);
-			interfaze.addMethod(method);
-		}
-	}
 
 	public void addMapperAnnotations(Interface interfaze, Method method) {
 	}
